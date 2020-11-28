9796a37c08de15617e2e8304b2256a0c41ae3379
Daniel Gray <the.daniel.gray@gmail.com>
2020-11-28 13:17:52 +0100

    Improve logging of the facilities refresher

--- src/main/java/app/trufi/mobidatabw/FacilitiesLoader.java
+++ src/main/java/app/trufi/mobidatabw/FacilitiesLoader.java
@@ -2,12 +2,15 @@ package app.trufi.mobidatabw;
 
 import app.trufi.mobidatabw.model.Facility;
 import org.springframework.beans.factory.annotation.Value;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.lang.invoke.MethodHandles;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
@@ -15,6 +18,7 @@ import java.util.Optional;
 @Component
 public class FacilitiesLoader {
 
+	Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 	List<Facility> cachedFacilities;
 
 	@Value("${accessToken}")
@@ -22,15 +26,21 @@ public class FacilitiesLoader {
 
 	@Scheduled(fixedRate=60*1000)
 	public void refreshFacilities() {
+		logger.info("Refreshing the facilities from the Deustchebahn API...");
 		WebClient dbApiClient = WebClient.builder()//
 				.baseUrl("https://api.deutschebahn.com")
 				.defaultHeader("Accept", "application/json")//
 				.defaultHeader("Authorization", "Bearer " + accessToken)
 				.build();
 		WebClient.RequestBodySpec facilitiesReq = dbApiClient.method(HttpMethod.GET).uri(URI.create("https://api.deutschebahn.com/fasta/v2/facilities"));
-		ResponseEntity<List<Facility>> response = facilitiesReq.retrieve().toEntityList(Facility.class).block();
-		assert response != null;
-		this.cachedFacilities = response.getBody();
+		try {
+			ResponseEntity<List<Facility>> response = facilitiesReq.retrieve().toEntityList(Facility.class).block();
+			assert response != null;
+			this.cachedFacilities = response.getBody();
+			logger.info("Facilities refreshed successfully");
+		} catch (Exception ex) {
+			logger.error("An error occured while refreshing the facilities", ex);
+		}
 	}
 
 	public List<Facility> getCachedFacilities() {
